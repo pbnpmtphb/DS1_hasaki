87d79c99226ae5c7b0ab559ad59f1b9142322be2
Nhật Phạm <89813639+pbnpmtphb@users.noreply.github.com>
2024-12-13 06:48:47 +0700

    test_preprocessing.py

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -34,27 +34,29 @@ def process_text(text, emoji_dict, teen_dict, english_dict, wrong_lst):
     return document
 
 def process_special_word(text):
-    # có thể có nhiều từ đặc biệt cần ráp lại với nhau
-    new_text = ''
+    # List of special words that need to be combined with the next word
+    special_words = ['không','không như', 'chẳng', 'chả','khó','chưa','đáng','giảm','kém']
+    
+    new_text_lst = []  # Using a list to build the result
     text_lst = text.split()
-    i= 0
-    # không, chẳng, chả...
-    if 'không' in text_lst:
-        while i <= len(text_lst) - 1:
-            word = text_lst[i]
-            #print(word)
-            #print(i)
-            if  word == 'không':
-                next_idx = i+1
-                if next_idx <= len(text_lst) -1:
-                    word = word +'_'+ text_lst[next_idx]
-                i= next_idx + 1
-            else:
-                i = i+1
-            new_text = new_text + word + ' '
-    else:
-        new_text = text
-    return new_text.strip()
+    i = 0
+    
+    while i < len(text_lst):
+        word = text_lst[i]
+        
+        # Check if the current word is in the list of special words
+        if word in special_words:
+            # If there is a next word, merge the special word with it
+            if i + 1 < len(text_lst):
+                word = word + '_' + text_lst[i + 1]
+                i += 1  # Skip the next word as it has been merged
+        new_text_lst.append(word)
+        i += 1  # Move to the next word
+    
+    # Join the words back into a single string
+    new_text = ' '.join(new_text_lst)
+    
+    return new_text
 
 def process_postag_thesea(text):
     new_document = ''
@@ -73,3 +75,31 @@ def remove_stopwords(text, stopword_lst):
     words = text.split()  # Split the text into words
     filtered_words = [word for word in words if word not in stopword_lst]  # Remove stopwords
     return ' '.join(filtered_words)  # Join the filtered words back into a string
+
+# Chuẩn hóa unicode tiếng việt
+def loaddicchar():
+    uniChars = "àáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệđìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶÈÉẺẼẸÊỀẾỂỄỆĐÌÍỈĨỊÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢÙÚỦŨỤƯỪỨỬỮỰỲÝỶỸỴÂĂĐÔƠƯ"
+    unsignChars = "aaaaaaaaaaaaaaaaaeeeeeeeeeeediiiiiooooooooooooooooouuuuuuuuuuuyyyyyAAAAAAAAAAAAAAAAAEEEEEEEEEEEDIIIOOOOOOOOOOOOOOOOOOOUUUUUUUUUUUYYYYYAADOOU"
+
+    dic = {}
+    char1252 = 'à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ'.split(
+        '|')
+    charutf8 = "à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ".split(
+        '|')
+    for i in range(len(char1252)):
+        dic[char1252[i]] = charutf8[i]
+    return dic
+
+# Đưa toàn bộ dữ liệu qua hàm này để chuẩn hóa lại
+def covert_unicode(txt):
+    dicchar = loaddicchar()
+    return regex.sub(
+        r'à|á|ả|ã|ạ|ầ|ấ|ẩ|ẫ|ậ|ằ|ắ|ẳ|ẵ|ặ|è|é|ẻ|ẽ|ẹ|ề|ế|ể|ễ|ệ|ì|í|ỉ|ĩ|ị|ò|ó|ỏ|õ|ọ|ồ|ố|ổ|ỗ|ộ|ờ|ớ|ở|ỡ|ợ|ù|ú|ủ|ũ|ụ|ừ|ứ|ử|ữ|ự|ỳ|ý|ỷ|ỹ|ỵ|À|Á|Ả|Ã|Ạ|Ầ|Ấ|Ẩ|Ẫ|Ậ|Ằ|Ắ|Ẳ|Ẵ|Ặ|È|É|Ẻ|Ẽ|Ẹ|Ề|Ế|Ể|Ễ|Ệ|Ì|Í|Ỉ|Ĩ|Ị|Ò|Ó|Ỏ|Õ|Ọ|Ồ|Ố|Ổ|Ỗ|Ộ|Ờ|Ớ|Ở|Ỡ|Ợ|Ù|Ú|Ủ|Ũ|Ụ|Ừ|Ứ|Ử|Ữ|Ự|Ỳ|Ý|Ỷ|Ỹ|Ỵ',
+        lambda x: dicchar[x.group()], txt)
+
+import re
+# Hàm để chuẩn hóa các từ có ký tự lặp
+def normalize_repeated_characters(text):
+    # Thay thế mọi ký tự lặp liên tiếp bằng một ký tự đó
+    # Ví dụ: "lònggggg" thành "lòng", "thiệtttt" thành "thiệt"
+    return re.sub(r'(.)\1+', r'\1', text)
